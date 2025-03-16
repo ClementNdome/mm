dddb5bdb1d75c4700f52fa0b09e4464ff192d59e
ClementNdome <clementndome20@gmail.com>
2025-03-17 02:04:26 +0300

    removed transition time

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { picture,picture1,picture2,picture3,picture4} from '../assets';
-import { Link } from 'react-router-dom'; 
+import { picture, picture1, picture2, picture3, picture4 } from '../assets';
+import { Link } from 'react-router-dom';
 import SectionWrapper from './SectionWrapper';
-// Add your own images by putting them in the assets folder and import them.
-const images = [
- picture,
- picture1,
- picture2,
- picture3,
- picture4,
 
-];
+const images = [picture, picture1, picture2, picture3, picture4];
+
 function Picture() {
   const [loadedImages, setLoadedImages] = useState(0);
 
   const handleImageLoad = () => {
     setLoadedImages((prev) => prev + 1);
   };
+
   const allImagesLoaded = loadedImages === images.length;
+
   return (
     <SectionWrapper>
       <Link to="/card">
@@ -34,9 +30,7 @@ function Picture() {
       {images.map((image, index) => (
         <motion.div
           key={index}
-          className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ${
-            allImagesLoaded ? 'opacity-100' : 'opacity-0'
-          }`}
+          className={`absolute top-0 left-0 w-full h-full ${allImagesLoaded ? 'opacity-100' : 'opacity-0'}`}
           style={{
             zIndex: images.length - index,
           }}
@@ -54,7 +48,7 @@ function Picture() {
             src={image}
             alt={`Stacked image ${index + 1}`}
             className="w-full h-full object-cover rounded-lg shadow-lg"
-            onLoad={handleImageLoad} // Increment the counter when the image loads
+            onLoad={handleImageLoad}
           />
         </motion.div>
       ))}
